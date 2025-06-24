671da6bb5c786b48f8e4f9614ce54ccee2811682
ShashankkY <shashankyogesh12@gmail.com>
2025-06-24 20:58:59 +0530

    Update HelloString.java

--- HelloString.java
+++ HelloString.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class HelloString 
 {
     public static void main(String[] args)
@@ -5,7 +8,11 @@ public class HelloString
         String name = "Shashank";
         String message = "Welcome to java programming!";
         String fullMessage = "Hello, " + name + "! " + message;
+        LocalDateTime currentTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedDateTime = currentTime.format(formatter);
         System.out.println(fullMessage);
         System.out.println("Your name has " + name.length() +  "characters.");  
+        System.out.println("Current date and time: " + formattedDateTime);
     }
 }
