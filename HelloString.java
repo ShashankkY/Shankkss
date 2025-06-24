17b892333a831b5e510273ef48525d9a954131dc
ShashankkY <shashankyogesh12@gmail.com>
2025-06-24 20:37:49 +0530

    Create HelloString.java

---
+++ HelloString.java
@@ -0,0 +1,11 @@
+public class HelloString 
+{
+    public static void main(String[] args)
+    {
+        String name = "Shashank";
+        String message = "Welcome to java programming!";
+        String fullMessage = "Hello, " + name + "! " + message;
+        System.out.println(fullMessage);
+        System.out.println("Your name has " + name.length() +  "characters.");  
+    }
+}
